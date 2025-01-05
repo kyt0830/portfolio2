401e0491d959a17a3349d390419391a199e9affd
kyt0830 <kyt910830@gmail.com>
2025-01-05 20:09:36 +0900

    header.js 추가

--- js/header.js
+++ js/header.js
@@ -1,23 +1,8 @@
-//header.js
 
 
-
-
-$(document).ready(function(){
-    //header에 사용되는 jq
-    $('.gnb').mouseenter(function () {
-        $('.lnb, .bgcolor').stop().slideDown(200);
-    });
-    $('nav').mouseleave(function () {
-        $('.lnb, .bgcolor').stop().slideUp(200);
-    });
-    // searchbox 높이는 keywords의 높이값을 받아와서 늘어나도록
-    // keywords 누르면 input으로 값 전달하고 제출까지 되도록
-    $('.search').click(function () {
-        $('.search-box').stop().slideToggle(200);
+// header.js
+$(document).ready(function () {
+    $('.ham').click(function () {
+        $('.gnb').slideToggle(200);
     });
 });
-
-
-
-
